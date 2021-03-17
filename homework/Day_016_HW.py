c63f7371bb7990d9d1058146b68a5196002e02b8
Shijung Huang <shijung.huang@skylinetw.com>
2021-03-17 09:46:39 +0800

    new: Day_016_HW

---
+++ homework/Day_016_HW.py
@@ -0,0 +1,11 @@
+import pandas as pd
+index = pd.date_range("1/1/2019", periods=20, freq='D')
+series = pd.Series(range(20), index=index)
+# print(index)
+# print(series)
+#1. 將所有轉為周資料
+week_seris = series.to_period('W')
+week_seris
+#2. 將周資料的值取平均
+mean_week = week_seris.mean()
+mean_week
